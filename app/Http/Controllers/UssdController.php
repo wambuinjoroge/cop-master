9221a9f1d9c32910987df91889f6aef5750732be
mancini85 <fredmancini85@gmail.com>
2017-11-23 09:07:38 +0300

    Modified USSD Controller  features

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -18,23 +18,23 @@ class UssdController extends Controller
 		if($input == "")
 		{
 		
-			$response = "CON Welcome to ecp\n Please Select Service\n Press:\n";
-			$response .= "1 Please visit www.ecp.co.ke\n";
-			$response .= "2 Inquire Information\n";
-			$response .= "3 Customer Care\n";
-			$response .= "4 Unsubscribe\n";
-			$response .= "5 Subscribe\n";
-			$response .= "6 Quit ";
+			$response = "CON Welcome to ecp\n Please Select Service\n";
+			$response .= "1:Please visit www.ecp.co.ke\n";
+			$response .= "2:Inquire Information\n";
+			$response .= "3:Customer Care\n";
+			$response .= "4:Unsubscribe\n";
+			$response .= "5:Subscribe\n";
+			$response .= "6:Quit ";
 
 		}
 		else if($input == "1" || $input=="2")
 		{
 			$response = "CON Choose Option\n";
-			$response .= "1 Verify Information\n";
-			$response .= "2 Check Logs\n";
-			$response .= "3 Search Contact\n";
-			$response .= "0 BACK\n";
-			$response .= "00 HOME";
+			$response .= "1:Verify Information\n";
+			$response .= "2:Check Logs\n";
+			$response .= "3:Search Contact\n";
+			$response .= "0:BACK\n";
+			$response .= "00:HOME";
 		}
 		else if($input =="1*1" || $input =="1*2" || $input =="1*3" || $input =="2*1" || $input =="2*2" || $input =="2*3")
 		{
