7f081c02f6bf435422dd871bfea743795b102ced
mancini85 <fredmancini85@gmail.com>
2017-11-23 08:56:43 +0300

    Modified USSD Controller  features

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -17,22 +17,24 @@ class UssdController extends Controller
 
 		if($input == "")
 		{
-		"Welcome to ecp\n";
-			$response = "CON Respond using options\n";
-			"Press:\n";
+		
+			$response = "CON Welcome to ecp\n Please Select Service\n Press:\n";
 			$response .= "1 Please visit www.ecp.co.ke\n";
 			$response .= "2 Inquire Information\n";
 			$response .= "3 Customer Care\n";
-			$response .= "4 Unsubscribe \n";
-			$response .= "5 Subscribe ";
+			$response .= "4 Unsubscribe\n";
+			$response .= "5 Subscribe\n";
+			$response .= "6 Quit ";
 
 		}
 		else if($input == "1" || $input=="2")
 		{
-			$response = "CON Please Select Service\n";
+			$response = "CON Choose Option\n";
 			$response .= "1 Verify Information\n";
 			$response .= "2 Check Logs\n";
-			$response .= "3 Search Contact";
+			$response .= "3 Search Contact\n";
+			$response .= "0 BACK\n";
+			$response .= "00 HOME";
 		}
 		else if($input =="1*1" || $input =="1*2" || $input =="1*3" || $input =="2*1" || $input =="2*2" || $input =="2*3")
 		{
