867330e4edd7aa45976ea34968c0238e9438c302
mancini85 <fredmancini85@gmail.com>
2017-11-25 13:47:27 +0300

    Modified welcome page  features

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,7 +137,7 @@
                                     <h4>Efficient</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="lead-in">Its secure , easy and fast.</p>
+                                    <p class="lead-in">It's secure , easy and fast.</p>
                                 </div>
                             </div>
                 </div>
@@ -165,7 +165,7 @@
                                     <h4> Alerts</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="lead-in">Receive instant SMS and email alerts for confirmation and status.</p>
+                                    <p class="lead-in">Receive instant alerts for<br>  confirmation and status.</p>
                                 </div>
                             </div>
                 </div>
@@ -179,7 +179,7 @@
                                     <h4>Online Services</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="lead-in">Fill online application forms, submit then receive feedback on Any device .</p>
+                                    <p class="lead-in">Fill online application forms,submit then get feedback on Any device .</p>
                                 </div>
                             </div>
                 </div>
